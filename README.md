bfc6f3264e5ca9d1384a7bf14856d23bd2a97176
Michael-Meade <mameade@utica.edu>
2025-01-05 15:34:34 -0500

    added fun.rb... added stuff to the readme.md...

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ gem install CryptoPriceFinder
 
 ## Usage
 
-This shows the basic usage of the gem. It can get the prices of crypto in USD. 
+This shows the basic usage of the gem. It can get the prices of crypto in USD of over 30 different cryptocurrencies. Examples of usage can also be found in the `exmaples` folder.
 
 ```ruby
 puts "Monero:\s"      + CryptoPriceFinder::monero(6.6).to_s
@@ -60,17 +60,13 @@ usd_xmr = CryptoPriceFinder::monero(xmr)
 btc = 0.103
 usd_btc = CryptoPriceFinder::bitcoin(btc)
 
-
 xlm = 6014
 usd_xlm = CryptoPriceFinder::stellar(xlm)
-
 
 total = usd_xmr + usd_btc + usd_xlm
 xlm_percent = usd_xlm.to_f / total * 100.0
 xmr_percent = usd_xmr.to_f / total * 100.0
 btc_percent = usd_btc.to_f / total * 100.0
-
-
 
 puts "XLM Hodling:\s#{xlm_percent.round(3)}%"
 puts "XMR Hodling:\s#{xmr_percent.round(3)}%"
---
+++ examples/fun.rb
@@ -0,0 +1,107 @@
+
+require 'CryptoPriceFinder'
+
+menu = "
+1) Get price on Monero
+2) Get price on Stellar
+3) Get price on Bitcoin
+4) Get price on Dogecoin
+5) Get price on Ethereum
+6) Get price on Ripple
+7) Get price on Tron
+8) Get price on Tether
+9) Get price on Solana
+10) Get price on BitcoinCash
+11) Get price on Pepe
+12) Get price on LiteCoin
+13) Get price on Polkadot
+14) Get price on ChainLink
+15) Get price on Shiba Inu
+16) Get price on FileCoin
+17) Get price on BAT
+18) Get price on BNB
+19) Get price on Bonk
+20) Get price on RavenCoin
+21) Get price on USDC
+22) Get price on Cardano
+23) Get price on SUI
+24) Get price on uniswap
+25) Get price on fantom
+26) Get price on FartCoin
+27) Get price on NEO
+28) Get price on FTK Token
+29) Get price on Flow
+30) Get price on WorldCoin
+31) Get price on Vechain
+\n"
+
+print(menu)
+print("Enter Number:")
+num = gets.chomp.to_i
+print("Enter amount of Crypto:")
+amount = gets.chomp
+print("\n\n\n\n")
+case num
+when 1
+	puts CryptoPriceFinder::monero(amount)
+when 2
+	puts CryptoPriceFinder::stellar(amount)
+when 3
+	puts CryptoPriceFinder::bitcoin(amount)
+when 4
+	puts CryptoPriceFinder::dogecoin(amount)
+when 5
+	puts CryptoPriceFinder::ethereum(amount)
+when 6
+	puts CryptoPriceFinder::ripple(amount)
+when 7
+	puts CryptoPriceFinder::tron(amount)
+when 8
+	puts CryptoPriceFinder::tether(amount)
+when 9 
+	puts CryptoPriceFinder::solana(amount)
+when 10
+	puts CryptoPriceFinder::bitcoin_cash(amount)
+when 11
+	puts CryptoPriceFinder::pepe(amount)
+when 12
+	puts CryptoPriceFinder::litecoin(amount)
+when 13
+	puts CryptoPriceFinder::polkadot(amount)
+when 14
+	puts CryptoPriceFinder::chainlink(amount)
+when 15
+	puts CryptoPriceFinder::shiba_inu(amount)
+when 16
+	puts CryptoPriceFinder::filecoin(amount)
+when 17
+	puts CryptoPriceFinder::bat(amount)
+when 18
+	puts CryptoPriceFinder::bnb(amount)
+when 19
+	puts CryptoPriceFinder::bonk(amount)
+when 20
+	puts CryptoPriceFinder::ravencoin(amount)
+when 21
+	puts CryptoPriceFinder::usdc(amount)
+when 22
+	puts CryptoPriceFinder::cardano(amount)
+when 23
+	puts CryptoPriceFinder::sui(amount)
+when 24
+	puts CryptoPriceFinder::uniswap(amount)
+when 25
+	puts CryptoPriceFinder::fantom(amount)
+when 26
+	puts CryptoPriceFinder::fartcoin(amount)
+when 27
+	puts CryptoPriceFinder::neo(amount)
+when 28
+	puts CryptoPriceFinder::ftx_token(amount)
+when 29
+	puts CryptoPriceFinder::flow(amount)
+when 30
+	puts CryptoPriceFinder::worldcoin(amount)
+when 31
+	puts CryptoPriceFinder::vechain(amount)
+end
